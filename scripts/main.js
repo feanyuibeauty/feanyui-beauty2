962c4d2c21f17eb4b37da5164f080bded865b588
feanyuibeauty <feanyuibeauty@gmail.com>
2025-07-11 02:23:42 -0500

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('#menu-toggle');
-    const navLinks = document.querySelector('nav');
-
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-    });
-});
